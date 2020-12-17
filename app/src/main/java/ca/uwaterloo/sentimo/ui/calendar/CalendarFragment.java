4f5b3d64c34e4006bbbdd1f8ea8bdac4e2310d03
Ethan Kim <robhn38426@gmail.com>
2020-12-16 22:37:00 -0800

    Changed notifications to calendar

--- app/src/main/java/ca/uwaterloo/sentimo/ui/calendar/CalendarFragment.java
+++ app/src/main/java/ca/uwaterloo/sentimo/ui/calendar/CalendarFragment.java
@@ -1,4 +1,4 @@
-package ca.uwaterloo.sentimo.ui.notifications;
+package ca.uwaterloo.sentimo.ui.calendar;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import ca.uwaterloo.sentimo.R;
 
-public class NotificationsFragment extends Fragment {
+public class CalendarFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private CalendarViewModel calendarViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        calendarViewModel =
+                new ViewModelProvider(this).get(CalendarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_calendar, container, false);
+        final TextView textView = root.findViewById(R.id.text_calendar);
+        calendarViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/ca/uwaterloo/sentimo/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/ca/uwaterloo/sentimo/ui/calendar/CalendarViewModel.java
@@ -1,16 +1,16 @@
-package ca.uwaterloo.sentimo.ui.notifications;
+package ca.uwaterloo.sentimo.ui.calendar;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class CalendarViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public CalendarViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is the calendar fragment");
     }
 
     public LiveData<String> getText() {
