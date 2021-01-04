bd99ed01bc436b43ccb35ba49512bb9ed5bc658a
Ethan Kim <robhn38426@gmail.com>
2021-01-03 17:10:14 -0800

    record in progress

--- app/src/main/java/ca/uwaterloo/sentimo/RecordActivity.java
+++ app/src/main/java/ca/uwaterloo/sentimo/RecordActivity.java
@@ -3,33 +3,50 @@ package ca.uwaterloo.sentimo;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import java.io.IOException;
+
 public class RecordActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String LOG_TAG = "AudioRecordTest";
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static String outputName = null;
 
     private ImageButton recordBtn;
+    private TextView recordTxt;
+    private Chronometer TimerChr;
+
+    private MediaRecorder mRecorder;
+
     private boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
         recordBtn = findViewById(R.id.btn_record);
         recordBtn.setImageDrawable(getDrawable(R.drawable.record_icon_off));
         recordBtn.setOnClickListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        outputName = Environment.getExternalStorageDirectory().getAbsolutePath();
+        outputName += "/AudioRecording.3gp";
     }
 
     @Nullable
@@ -55,14 +72,52 @@ public class RecordActivity extends AppCompatActivity implements View.OnClickLis
         }
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        startActivity(new Intent(RecordActivity.this, MainActivity.class));
+        return true;
+    }
+
     private boolean checkPermissions() {
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         return true;
     }
 
+    private void onRecord(boolean start) {
+        if (start)
+            startRecording();
+        else
+            stopRecording();
+    }
+
+    private void startRecording() {
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(outputName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        startActivity(new Intent(RecordActivity.this, MainActivity.class));
-        return true;
+    protected void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
     }
 }
